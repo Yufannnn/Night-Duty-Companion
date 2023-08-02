822b9d3bc5f8df4c60a88938b666ec4df9451ada
Yufannnn <e0773591@u.nus.edu>
2023-08-02 11:55:41 +0800

    website able to take csv

---
+++ website/src/UIscript.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed("#typed-text", {
+      strings: ["Night Duty Companion"],
+      typeSpeed: 80,
+      startDelay: 1000,
+      loop: false
+    });
+  });
+
+  
